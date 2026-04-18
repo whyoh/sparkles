33e07dcdc4ad537817933c730d562c750aaeb95b
whyoh <whyoh@entrophilia.com>
2026-04-18 01:10:38 +0100

    transparent, more equilateral

--- sparkles.py
+++ sparkles.py
@@ -2,9 +2,9 @@ import pyglet
 import random
 
 
-how_many_sparkles = 100
-speed = 100
-size = 50
+how_many_sparkles = 250
+speed = 50
+size = 80
 
 window = pyglet.window.Window(fullscreen=True, config=pyglet.gl.Config(double_buffer=True))
 
@@ -12,23 +12,23 @@ sparkles = []
 for _ in range(how_many_sparkles):
     sparkles.append(pyglet.shapes.Triangle(
         window.width//2, window.height//2,
-        window.width//2 + size/2, window.height//2 + size / 2 ** 0.5,
+        window.width//2 + size/2, window.height//2 + size * 0.75 ** 0.5,
         window.width//2 + size, window.height//2))
+
+
+def bump():
+    for t in sparkles:
+        t.color = (random.randint(150, 250), random.randint(150, 250), random.randint(150, 250), random.randint(0, 250))
+        t.x = (t.x + random.randint(-speed//2, speed//2)) % window.width
+        t.y = (t.y + random.randint(-speed//2, speed//2)) % window.height
+        t.rotation += random.randint(-speed, speed)
 
 
 @window.event
 def on_draw():
     window.clear()
     [t.draw() for t in sparkles]
+    bump()
 
 
-def bump(x):
-    for t in sparkles:
-        t.color = (random.randint(150, 250), random.randint(150, 250), random.randint(150, 250))
-        t.x = (t.x + random.randint(-speed//2, speed//2)) % window.width
-        t.y = (t.y + random.randint(-speed//2, speed//2)) % window.height
-        t.rotation += random.randint(-speed, speed)
-
-
-pyglet.clock.schedule_interval(bump, 0.01)
 pyglet.app.run()
